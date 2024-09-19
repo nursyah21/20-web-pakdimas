838a974060503ad11625cc9003973f6e8082762d
nursyah21 <nursyahjaya@gmail.com>
2024-09-19 20:07:24 +0700

    header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,33 @@
 import { createSignal } from 'solid-js'
 import solidLogo from '../assets/solid.svg'
 import viteLogo from '/vite.svg'
-
+import Banner from "../assets/banner.jpg"
+import Logo from "../assets/logo.png"
+import Search from "../assets/search.svg"
 
 export function Index() {
   const [count, setCount] = createSignal(0)
 
   return (
-    <div class='bg-black min-h-screen flex text-white justify-center flex-col items-center'>
-      <div class='flex'>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="w-24" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="w-24" alt="Solid logo" />
-        </a>
-      </div>
-      <h1 class='font-bold my-4 text-2xl'>Vite + Solid</h1>
-      <div class="text-center">
-        <button class='p-2 px-4 bg-blue-600 rounded-xl hover:opacity-80' onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
+    <div class=''>
+      <div class='fixed top-0 text-white flex items-center justify-between w-full p-2'>
+        <div class='flex items-center'>
+          <img src={Logo} alt="" />
+          <h1 class='font-bold text-xl'>PT Summit Vision Nusantara</h1>
+        </div>
+        <div class='items-center flex text-slate-200 gap-x-4'>
+          <a href="/" >Home</a>
+          <a href="/products">Products and Services</a>
+          <a href="/partner">Partner</a>
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
 
+          </button>
+        </div>
       </div>
-      <div class='text-center mt-4 text-sm'>
-        <p>
-          Edit <span class='underline text-slate-200'>src/App.tsx</span> and save to test HMR
-        </p>
-        <p class="read-the-docs">
-          Click on the Vite and Solid logos to learn more
-        </p>
-      </div>
+      <img src={Banner} alt="" class='w-full' />
     </div>
   )
 }
