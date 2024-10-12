42c2dc252136aba46ce4b0655cdbb187f2cbf2c0
nursyah21 <nursyahjaya@gmail.com>
2024-10-13 00:13:49 +0700

    update home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ export default {
     extend: {
       colors: {
         header: "rgba(33,33,33,1)",
-        about: "rgba(232,232,232,255)"
+        about: "rgba(232,232,232,255)",
+        bluebg: "rgba(13,19,55,1)",
+        bluePortfolio: "#1c4587"
       }
     },
   },
