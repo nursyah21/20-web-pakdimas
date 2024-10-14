2c7fb02f96e9609a35e90aaa94d15a786295211f
nursyah21 <nursyahjaya@gmail.com>
2024-10-14 21:00:37 +0700

    update navbar to use navlink and make layout component

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './pages/Index.tsx'
+import Home from './pages/Index.tsx'
+import ProductAndServices from './pages/ProductAndServices.tsx'
+import Partner from './pages/Partner.tsx'
+import Support from './pages/Support.tsx'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
@@ -9,7 +12,10 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' Component={App} />
+        <Route path='/' Component={Home} />
+        <Route path='/products-and-services' Component={ProductAndServices} />
+        <Route path='/partner' Component={Partner} />
+        <Route path='/support' Component={Support} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
