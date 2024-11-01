05367ef091cd95b003f921209174f94ccb6ed90f
nursyah21 <nursyahjaya@gmail.com>
2024-11-01 18:38:27 +0700

    seadanya dulu

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Index.tsx'
 import ProductAndServices from './pages/ProductAndServices.tsx'
 import Partner from './pages/Partner.tsx'
 import Support from './pages/Support.tsx'
+import Product from './pages/Product.tsx'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path='/products-and-services' Component={ProductAndServices} />
         <Route path='/partner' Component={Partner} />
         <Route path='/support' Component={Support} />
+        <Route path='/product/:id' Component={Product} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
